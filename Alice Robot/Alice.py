981d8634bd5718b51ae858aee2ba7c1564230127
bob van den berg <bobberg90@hotmail.com>
2018-04-09 16:41:13 +0200

    Added functionality for Alice in one folder. Still needs to be editted for the workshop. Is connected, working and tested with robot.

---
+++ Alice Robot/Alice.py
@@ -0,0 +1,51 @@
+'''
+
+Use example code if you want to use the Zora robot
+
+1 - You will need to install Python 2.7 - 32 bits (be sure that it is the 32 bits version)
+    You can find the installer here: https://www.python.org/downloads/release/python-2714/
+2 - After installing Python, install the naoqi executable also in Library: pynaoqi-python-2.7-naoqi-x.x-win32.exe
+
+'''
+
+import time
+import json
+from APIConnector import APIConnector
+from Zora import Zora
+
+
+class Alice:
+    def __init__(self):
+        self.answers = []
+
+        # This depends on what Zora robot you have. Make sure the robot is connected (through UTP-cable).
+        ALICE_NUMBER = "alice1"
+        self.ZORA_IP = "169.254.58.192"
+
+        # ALICE_NUMBER = "alice2"
+        # self.ZORA_IP = "169.254.58.192"
+
+        print("Starting Program for Alice 1")
+
+        # Stored credentials
+        self.creds = json.load(open('information.json'))
+        # Connect to the API with the given credentials
+        self.api = APIConnector(username=self.creds[ALICE_NUMBER]['username'],
+                                password=self.creds[ALICE_NUMBER]['password'])
+        # Connect to Zora
+        self.zora = Zora(self)
+
+        self.run()
+
+    def run(self):
+        self.zora.talk("Hey")
+
+    def onboarding(self):
+        self.zora.talk("Ik stel nu een vraag. Je kunt op deze vragen Ja of Nee antwoorden.")
+        for i, item in enumerate(self.creds["onboarding"]):
+            self.recognizedWord = ""
+            self.zora.recognize()
+            self.answers = self.recognizedWord
+            print(self.recognizedWord)
+
+Alice()
